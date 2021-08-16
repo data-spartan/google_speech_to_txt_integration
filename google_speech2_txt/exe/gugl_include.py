2e5a3a9b035f0eda7cc5a8b67319fc5922ed5449
data-spartan <62840751+data-spartan@users.noreply.github.com>
2021-08-16 09:15:14 +0200

    Add files via upload

---
+++ google_speech2_txt/exe/gugl_include.py
@@ -0,0 +1,7 @@
+#!/root/google_STT/env/bin/python3
+
+#######################################################################################################################                           
+	#script name		#input filepath			#output filepath	           #input file           #bucketname
+########################################################################################################################
+
+python3 google_exe.py /root/google_STT/google_speech_2_tx/input_audio/ /root/google_STT/google_speech_2_tx/output_files/ Mali_Princ_audio_knjiga.wav  ibis_speech_to_text_audio
